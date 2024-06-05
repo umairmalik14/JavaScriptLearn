02e0fa32e4cae381226b463f78995a38b1a024f8
Umair Faiz <102575644+umairmalik14@users.noreply.github.com>
2024-06-05 19:47:59 +0000

    Comparisons

---
+++ JS_Basics/Comparisonofdatatypes.js
@@ -0,0 +1,12 @@
+console.log(null > 0);
+console.log(null < 0);
+console.log(null == 0);
+
+console.log(undefined == 0);
+console.log(undefined < 0);
+console.log(undefined < 0);
+
+// ===  => strick  check not only value as well as datatype check..
+
+console.log("2" === 2);
+
